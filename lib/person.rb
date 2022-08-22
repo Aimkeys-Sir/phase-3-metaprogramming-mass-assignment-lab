5d428658016f7bf6ded30b194acc9ee67ca6f462
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-08-22 10:39:54 +0300

    commit tree 5b30bef44d6a1b849e273fe7cb16d7630aeed008

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,12 @@
 class Person
+  def initialize(person)
+      person.each do |key,value|
+        self.class.attr_accessor(key)
+        self.send("#{key}=",value)
+      end
+    end
   # your code here
 end
+
+me=Person.new({name:"Mk",age:24})
+pp me
